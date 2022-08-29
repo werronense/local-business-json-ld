5d143e7ac24e1f917de988f39ec165b15e5ada30
Stephen Werronen <stephen.werronen@gmail.com>
2022-08-29 16:13:47 -0700

    Correct typo

--- local-business-json-ld.php
+++ local-business-json-ld.php
@@ -4,7 +4,7 @@ Plugin Name: Local Business JSON-LD
 Description: The plugin adds schema.org structured data to every page of the site in JSON-LD format.
 Version: 1.0.0
 Author: Stephen Werronen
-License: GPL v2 or Later
+License: GPL v2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 Text Domain: local-business-json-ld
 */
